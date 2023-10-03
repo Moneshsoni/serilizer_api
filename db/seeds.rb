9b61a8b13c7e40f50b377738560b234076fd80d1
Moneshsoni <sonimonesh115@gmail.com>
2023-10-03 14:32:44 +0530

    add the serilizer in rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Employee.destroy_all
+Manager.destroy_all
+
+managers = (1..20).map do
+  Manager.create!(
+    name: "manager"
+  )
+end
+
+employee = (1..50).map do
+  Employee.create!(
+    name: "employee",
+    email: "emp@gmail.com",
+    dob: "17-01-1988",
+    mobile: "8879544321",
+    designation: "Senior Developer",
+    salary: 35_000,
+    manager: managers.sample
+  )
+end
